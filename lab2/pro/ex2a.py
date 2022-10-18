ce519292fc0f7c9aa1ac9183b91ffca94e323f29
18213857028@163.com <18213857028@163.com>
2022-10-18 18:10:20 +0800

    lab2

---
+++ lab2/pro/ex2a.py
@@ -0,0 +1,44 @@
+from sys import exit
+from bitcoin.core.script import *
+from bitcoin.wallet import CBitcoinSecret
+
+from utils import *
+from config import my_private_key, my_public_key, my_address, faucet_address
+from ex1 import send_from_P2PKH_transaction
+
+
+cust1_private_key = CBitcoinSecret(
+    'cTWkmp93Mbc28ydnwjzYfwgQ1G9a46uCfAT31X7BprCVXpcM74iz')
+cust1_public_key = cust1_private_key.pub
+cust2_private_key = CBitcoinSecret(
+    'cRB5ujRQ1TULCC5NSXyW7Xa4UtNfiggBJgFrKUfhwYo1eeZk9Jdk')
+cust2_public_key = cust2_private_key.pub
+cust3_private_key = CBitcoinSecret(
+    'cTcDFCNqMsJyCnrzyzkjo6ppbgXuLUuYpUWbLD8cjM4o2TSMVv4C')
+cust3_public_key = cust3_private_key.pub
+
+
+######################################################################
+# TODO: Complete the scriptPubKey implementation for Exercise 2
+
+# You can assume the role of the bank for the purposes of this problem
+# and use my_public_key and my_private_key in lieu of bank_public_key and
+# bank_private_key.
+
+ex2a_txout_scriptPubKey = [OP_0, my_public_key, cust1_public_key, cust2_public_key, cust3_public_key, OP_4, OP_CHECKMULTISIGVERIFY, my_public_key, OP_CHECKSIG];
+######################################################################
+
+if __name__ == '__main__':
+    ######################################################################
+    # TODO: set these parameters correctly
+    amount_to_send = 0.0012
+    txid_to_spend = (
+        'cc8fb122460f9f4cd88cd4f3b5f149c07421da36835e55dea30e8162b1e68be3')
+    utxo_index = 4
+    ######################################################################
+
+    response = send_from_P2PKH_transaction(
+        amount_to_send, txid_to_spend, utxo_index,
+        ex2a_txout_scriptPubKey)
+    print(response.status_code, response.reason)
+    print(response.text)
